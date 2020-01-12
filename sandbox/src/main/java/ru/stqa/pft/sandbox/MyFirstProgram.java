a0c582129fb282d0730a19fc92d819563a0e190d
yuliademidok <yul>
2020-01-12 18:46:49 +0300

    New methouds added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		hello("world");
+
+		double l = 10;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+	}
+
+	public static void hello (String somebody){
+		System.out.println("Hello " + somebody + "!");
+	}
+
+	public static double area (double l) {
+		return  l * l;
 	}
 }
