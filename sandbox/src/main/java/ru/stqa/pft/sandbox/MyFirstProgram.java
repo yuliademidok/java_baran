f582461a28eeb776dd7e5851e771951d63eaa0df
yuliademidok <yul>
2020-01-12 20:19:32 +0300

    testng

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,15 +4,14 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		hello("world");
 
-		double l = 10;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(10);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(2, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello (String somebody){
 		System.out.println("Hello " + somebody + "!");
-	}
-
-	public static double area (double l) {
-		return  l * l;
 	}
 }
