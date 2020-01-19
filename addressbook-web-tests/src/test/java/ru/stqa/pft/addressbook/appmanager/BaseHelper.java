a860a6b761d558590178e975c57d0835c3f93706
yuliademidok <yul>
2020-01-19 19:12:44 +0300

    BaseHelper update

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -15,8 +15,10 @@ public class BaseHelper {
 
     protected void type(By locator, String text) {
         click(locator);
-        driver.findElement((locator)).clear();
-        driver.findElement((locator)).sendKeys(text);
+        if(text != null) {
+            driver.findElement((locator)).clear();
+            driver.findElement((locator)).sendKeys(text);
+        }
     }
 
     public boolean isAlertPresent() {
